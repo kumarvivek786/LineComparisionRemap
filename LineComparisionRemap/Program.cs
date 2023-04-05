4fd6957b62e0c17d82f6b8d77d15af19cb8f0770
vivek <ikumarvivek786@gmail.com>
2023-04-05 20:21:49 +0530

    [Vivek]Compare Two Lines]

--- LineComparisionRemap/Program.cs
+++ LineComparisionRemap/Program.cs
@@ -11,13 +11,11 @@ namespace LineComparisionRemap
         static void Main(string[] args)
         {
 
-            //UC-1 Calculate the Length of Line
-            
-            int x1;
-            int y1;
-            int x2;
-            int y2;
-            double Length;
+            //UC-2 Find two lines are equal or not
+
+            int x1, y1, x2, y2, x3, y3, x4, y4;
+            double Length1;
+            double Length2;
 
 
             Console.WriteLine("x1 value:");
@@ -29,12 +27,31 @@ namespace LineComparisionRemap
             Console.WriteLine("y2 value:");
             y2 = Convert.ToInt32(Console.ReadLine());
 
+
+
+            Console.WriteLine("x3 value:");
+            x3 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("y3 value:");
+            y3 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("x4 value:");
+            x4 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("y4 value:");
+            y4 = Convert.ToInt32(Console.ReadLine());
+
             //Formula
-            Length = Math.Sqrt((x2 - x1 ^ 2 + y2 - y1 ^ 2));
+            Length1 = Math.Sqrt((x2 - x1 ^ 2 + y2 - y1 ^ 2));
+            Length2 = Math.Sqrt((x4 - x3 ^ 2 + y4 - y4 ^ 2));
 
-            Console.WriteLine("Length of the Line is: " + Length);
+            if (Length1 == Length2)
+            {
+                Console.WriteLine("Lines are Equal");
+            }
+            else
+            {
+                Console.WriteLine("Lines are not Equal");
+            }
+
             Console.ReadLine();
-
         }
     }
 }
